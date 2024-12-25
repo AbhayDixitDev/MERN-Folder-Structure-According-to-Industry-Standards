dc0a0947d79959055ac9361d3a191cb701c1d5ac
Abhay Dixit <dixitshivam534@gmail.com>
2024-12-25 15:17:16 +0530

    Utiloties systematic api error & response  with asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
     path: "./.env"
 });
-connectDB();
+connectDB()
+.then(() =>{
+    app.on("error", (err) => {
+        console.log("Server error", err);
+    });
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch(err => console.log("MongoDB connection failed !!!", err));
